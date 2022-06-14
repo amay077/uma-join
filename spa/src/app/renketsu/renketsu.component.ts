23745a3a2da2ad9a5524618b97e04a66204efc81
amay077 <okuokuoku@gmail.com>
2022-06-14 22:15:17 +0900

    fix

--- spa/src/app/renketsu/renketsu.component.ts
+++ spa/src/app/renketsu/renketsu.component.ts
@@ -119,6 +119,7 @@ export class RenketsuComponent implements OnInit {
       return;
     }
 
+    this.progress = 0;
     this.processing = true;
     this.imageBlob = null;
     this.imageSrc = '';
